d67d66d77c9f81c98c51ba10cdd236145ab359e1
Damien Angelos <bostaunieux@gmail.com>
2025-04-05 13:57:17 -0400

    more pretteier cleanup

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,15 +1,15 @@
 function debounce<T extends unknown[], U>(
-    callback: (...args: T) => PromiseLike<U> | U,
-    wait: number
-  ) {
-    let timer: number;
-  
-    return (...args: T): Promise<U> => {
-      clearTimeout(timer);
-      return new Promise((resolve) => {
-        timer = window.setTimeout(() => resolve(callback(...args)), wait);
-      });
-    };
-  }
+  callback: (...args: T) => PromiseLike<U> | U,
+  wait: number,
+) {
+  let timer: number;
+
+  return (...args: T): Promise<U> => {
+    clearTimeout(timer);
+    return new Promise((resolve) => {
+      timer = window.setTimeout(() => resolve(callback(...args)), wait);
+    });
+  };
+}
 
 export default debounce;
